81af0599e5f20af9f64cb2b499758933100febf2
Imran Hossen <freelancerimran81@gmail.com>
2022-03-30 21:32:20 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import Orders from './components/Orders/Orders';
 import Shop from './components/Shop/Shop';
 
 function App() {
@@ -10,9 +12,10 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Shop></Shop>} ></Route>
-        <Route path='/home' element={<Shop/>}/>
+        <Route path='/shop' element={<Shop/>}/>
+        <Route path='/orders' element={<Orders/>}/>
+        <Route path='/inventory' element={<Inventory/>}/>
       </Routes>
-      <Shop></Shop>
     </div>
   );
 }
