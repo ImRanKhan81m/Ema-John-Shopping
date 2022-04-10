b05f1082f7c042ace9438896de1992528766735a
Imran Hossen <freelancerimran81@gmail.com>
2022-04-11 01:04:52 +0600

    form updated

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Login.css'
 
-const Login = () => {
+const SignUp = () => {
     return (
         <div className='form-container'>
             <div>
-                <h2 className='form-title'>Login</h2>
+                <h2 className='form-title'>Sign Up</h2>
                 <form>
                     <div className="input-group">
                         <label htmlFor='email'>Email</label>
-                        <input type="email" name="email" id="" required/>
+                        <input type="email" name="email" id="" />
                     </div>
                     <div className="input-group">
                         <label htmlFor='password'>Password</label>
+                        <input type="password" name="password" id=""  required/>
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor='confirm-password'>Confirm Password</label>
                         <input type="password" name="password" id="" required />
                     </div>
-                    <input className='form-submit' type="submit" value="Login" />
+                    <input className='form-submit' type="submit" value="Sign Up" />
                 </form>
                 <p>
-                    New to Ema-John? <Link className='form-link' to='/signup'>Create an account</Link>
+                    Already have an account? <Link className='form-link' to='/login'>Login</Link>
                 </p>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default SignUp;
