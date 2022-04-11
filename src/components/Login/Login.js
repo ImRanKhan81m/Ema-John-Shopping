af049a22ba7d8bbbc742e698c9dff053baaadc4f
Imran Hossen <freelancerimran81@gmail.com>
2022-04-11 14:09:26 +0600

    login page updated

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css'
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+ 
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+
+
+
     return (
         <div className='form-container'>
             <div>
@@ -10,11 +22,11 @@ const Login = () => {
                 <form>
                     <div className="input-group">
                         <label htmlFor='email'>Email</label>
-                        <input type="email" name="email" id="" required/>
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required/>
                     </div>
                     <div className="input-group">
                         <label htmlFor='password'>Password</label>
-                        <input type="password" name="password" id="" required />
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
                     </div>
                     <input className='form-submit' type="submit" value="Login" />
                 </form>
